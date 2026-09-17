f2e58ab5a6e4e0df791224dfa54dd95b6ca5c313
Thomas Turihamwe <tturihamwe@gmail.com>
2026-09-17 17:50:55 +0300

    Add invoice payment method

--- resources/views/pos/checkout.blade.php
+++ resources/views/pos/checkout.blade.php
@@ -303,7 +303,7 @@
         </div>
         <button type="button" id="receiptDoneBtn"
                 class="mt-3 w-full min-h-[44px] rounded-lg bg-indigo-600 px-4 py-2 text-sm font-semibold text-white hover:bg-indigo-700">
-            New sale
+            Back
         </button>
     </div>
 </div>
